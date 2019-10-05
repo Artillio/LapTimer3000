be85581d2f5e2836dbb453401bc7eb9e5478fdb3
lorenzocuoghi <42006664+lorenzocuoghi@users.noreply.github.com>
2019-10-05 19:02:29 +0200

    Arduino

---
+++ arrival_sensor/arrival_sensor.ino
@@ -0,0 +1,22 @@
+#define INPUT_PIN 2
+uint8_t found;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(INPUT_PIN, INPUT);
+  found = 1;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // 1 = oggetto assente
+  // 0 = oggetto presente
+  found = digitalRead(INPUT_PIN);
+
+  if (!found) {
+    Serial.print(found);
+    delay(100);
+  }
+}
